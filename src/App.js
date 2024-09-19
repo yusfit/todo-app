bad56063051f8047a03cceece017171ec2d666e9
fesomu yusuf <princefesomuyusuf@gmail.com>
2024-09-19 16:29:04 -0700

    modified

--- src/App.js
+++ src/App.js
@@ -45,21 +45,21 @@ function App() {
   });
   return (
     <div>
-      <div className="bg-white shadow-sm shadow-black md:mx-80 mx-4 md:mt-10 mt-20 pb-10 ">
+      <div className="bg-slate-700 h-screen shadow-sm shadow-black md:mx-80 mx-4 md:mt-10 mt-20 pb-10 ">
         <div className="mx-8 ">
           <div className="flex justify-between items-center pt-4">
-            <h1 className="text-3xl ">TODO-EASE</h1>
+            <h1 className="text-3xl text-white ">USE TODOS</h1>
             <img
-              src="https://img.freepik.com/free-photo/rag-doll-with-giant-magnifying-glass-question-mark_1156-263.jpg?ga=GA1.1.148381924.1699598539"
+              src="https://cdn3.iconfinder.com/data/icons/business-office/256/To-do_List-1024.png"
               alt=""
               className="h-12 w-12 object-contain"
             />
           </div>
           <hr className="mt-4" />
-          <h1 className="text-center text-xl p-2">What needs to be done?</h1>
+          <h1 className="text-center text-xl p-2">What are your tasks?</h1>
           <input
             type="text"
-            className="w-full h-12 border-2 border-blue-200 "
+            className="w-full h-12 border-2 border-blue-600 "
             placeholder="What needs to be done?"
             value={inputValue}
             onChange={(e) => setInputValue(e.target.value)}
@@ -74,7 +74,7 @@ function App() {
           <div className="flex mx-6 mt-2 justify-between gap-2 ">
             <button
               className={`border border-blue-200 w-full p-1 ${
-                filter === "all" ? "bg-blue-200" : ""
+                filter === "all" ? "bg-blue-600" : ""
               }`}
               onClick={() => setFilter("all")}
             >
@@ -82,7 +82,7 @@ function App() {
             </button>
             <button
               className={`border border-blue-200 w-full p-1 ${
-                filter === "active" ? "bg-blue-200" : ""
+                filter === "active" ? "bg-blue-600" : ""
               }`}
               onClick={() => setFilter("active")}
             >
@@ -91,7 +91,7 @@ function App() {
             </button>
             <button
               className={`border border-blue-200 w-full p-1 ${
-                filter === "completed" ? "bg-blue-200" : ""
+                filter === "completed" ? "bg-blue-600" : ""
               }`}
               onClick={() => setFilter("completed")}
             >
@@ -116,8 +116,8 @@ function App() {
           </div>
         </div>
       </div>
-      <p className="text-center">
-        Created by:
+      <p className="text-center mt-4">
+        Copyright:
         <a
           href="https://github.com/yusfit/todo-app"
           className="text-blue-600"
